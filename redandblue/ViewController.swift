6d8e063463668ba1cfdfb95a75b2aef4c00f50d6
Cary Leslie <caryleslie@MacBook-Air.local>
2016-01-14 20:27:20 -0500

    Added files for redandblue

--- redandblue/ViewController.swift
+++ redandblue/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var Bluebird: UIImageView!
+    @IBOutlet weak var Redbird: UIImageView!
+    @IBOutlet weak var RedButton: UIButton!
+    @IBOutlet weak var BlueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MakeBlueBirdDisappear(sender: AnyObject) {
+        Bluebird.hidden = true
+        Redbird.hidden = false
+    }
 
+    @IBAction func MakeRedBirdDisappear(sender: AnyObject) {
+        Bluebird.hidden = false
+        Redbird.hidden = true
+    }
 }
 
